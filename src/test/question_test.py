b4c6c25c15fd0ca87629170f65f3f64cb93f2ab9
BoyuHan666 <boyu.han@mail.mcgill.ca>
2024-07-26 21:16:45 +0800

    update structure

--- src/test/question_test.py
+++ src/test/question_test.py
@@ -2,6 +2,7 @@ import requests
 import json
 import sys
 
+
 def stream_chat(prompt, url):
     headers = {
         'Content-Type': 'application/json'
@@ -17,22 +18,23 @@ def stream_chat(prompt, url):
         print(f"Response content: {response.text}")
         return None
 
-    #sys.stdout.write("Response from model: ")
-    #sys.stdout.flush()
+    # sys.stdout.write("Response from model: ")
+    # sys.stdout.flush()
 
-    response_text = "" 
-    for line in response.iter_lines(): 
-        if line: decoded_line = line.decode('utf-8') 
-        if decoded_line.startswith('data:'): data = json.loads(decoded_line.split('data: ')[-1]) 
-        if 'done' in data and data['done']: break 
-        response_part = data.get('response', '') 
+    response_text = ""
+    for line in response.iter_lines():
+        if line: decoded_line = line.decode('utf-8')
+        if decoded_line.startswith('data:'): data = json.loads(decoded_line.split('data: ')[-1])
+        if 'done' in data and data['done']: break
+        response_part = data.get('response', '')
         if response_part:
-            readable_response = response_part.encode('utf-8').decode('unicode_escape') 
-            response_text += readable_response 
-            sys.stdout.write(readable_response) 
+            readable_response = response_part.encode('utf-8').decode('unicode_escape')
+            response_text += readable_response
+            sys.stdout.write(readable_response)
             sys.stdout.flush()
     print("\n")
     return response_text
+
 
 def test_chat(url, questions):
     results = []
@@ -40,7 +42,7 @@ def test_chat(url, questions):
     for question in questions:
         print(f"Question: {question}")
         response_text = stream_chat(question, url)
-        
+
         if response_text is not None:
             results.append({
                 "question": question,
@@ -49,14 +51,13 @@ def test_chat(url, questions):
 
     return results
 
+
 if __name__ == "__main__":
     url = "http://127.0.0.1:6006/chat"
-    
 
     questions = [
-        
-    ]
 
+    ]
 
     results = test_chat(url, questions)
 
